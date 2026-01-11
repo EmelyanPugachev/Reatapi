84d8cbcfcb0b1dd0e026f95c85517886282da9c4
EmelyanPugachev <akula.vladik@gmail.com>
2026-01-11 04:37:49 +0300

    new

--- main.go
+++ main.go
@@ -3,35 +3,53 @@ package main
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type requestBody struct {
+type RequestBody struct {
 	Task string `json:"task"`
 }
 
-var task string
-
-func getTask(c echo.Context) error {
-	return c.JSON(http.StatusOK, task)
+type Taska struct {
+	Task string `json:"task"`
+	ID   string `json:"id"`
 }
 
-func postTask(c echo.Context) error {
-	var body requestBody
+var tasks = []Taska{}
+
+func getTasks(c echo.Context) error {
+	return c.JSON(http.StatusOK, tasks)
+}
+
+func postTasks(c echo.Context) error {
+	var body RequestBody
 	if err := c.Bind(&body); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
 	}
-	task = body.Task
-	return c.JSON(http.StatusAccepted, task)
+	newtask := Taska{
+		Task: body.Task,
+		ID:   uuid.NewString(),
+	}
+	tasks = append(tasks, newtask)
+	return c.JSON(http.StatusAccepted, tasks)
+}
+
+func patchTask(c echo.Context) error {
+	var body RequestBody
+	if err := c.Bind(&body); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+	return c.JSON(http.StatusAccepted, tasks)
 }
 
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
-	e.GET("/", getTask)
-	e.POST("/", postTask)
+	e.GET("/", getTasks)
+	e.POST("/", postTasks)
 	e.Start("localhost:8080")
 
 }
